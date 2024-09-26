f29cc8f698c949c02446b15f287942a264f2ef75
tanujashelke <156294805+tanujashelke@users.noreply.github.com>
2024-09-26 17:38:40 +0530

    Add files via upload

---
+++ listdemo.py
@@ -0,0 +1,35 @@
+fruits=["apple","tomato","mango","cherry"]
+#print(fruits)
+#print(fruits[0])
+#print(fruits[1])
+"""fruits[2]="potato"
+print(fruits[2])
+print(fruits)"""
+
+"""fruits.append("mango")
+print(fruits)"""
+
+#fruits.extend("hello")
+print(fruits)
+
+"""Animal=["tiger","lion","fox","monkey"]
+num=[1,2,3,4,5,6,7,8,"hello"]
+
+#print(Animal+num)
+del Animal[2]
+print(Animal)
+Animal.remove("monkey")
+print(Animal)
+Animal.pop()
+print(Animal)"""
+del fruits[1]
+print(fruits)
+fruits.remove("cherry")
+print(fruits)
+fruits.pop()
+print(fruits)
+number=[12,34,56,76,43]
+#number.pop()
+print(number)
+number=[12]
+print(number)
