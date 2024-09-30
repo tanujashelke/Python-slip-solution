d4e1e415316b64cbb331a304079db5a3b110f0de
tanujashelke <156294805+tanujashelke@users.noreply.github.com>
2024-09-30 11:25:50 +0530

    Add files via upload

---
+++ slip28b.py
@@ -0,0 +1,6 @@
+lst1=[1,2,3,4]
+lst2=['pune','nashik','nagpur']
+t1=tuple(lst1)
+t2=tuple(lst2)
+t3=t1+t2
+print(t3)
---
+++ slip29a.py
@@ -0,0 +1,4 @@
+pi=3.14
+r=7
+V= 4.0/3.0*pi* r**3
+print("the volume of sphere is",V)
